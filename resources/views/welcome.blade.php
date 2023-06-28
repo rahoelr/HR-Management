81cefacb6922846233db386489fb30eead73fabc
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-06-28 17:09:31 +0700

    Test Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                                Sponsor Cek
                             </a>
                         </div>
                     </div>
