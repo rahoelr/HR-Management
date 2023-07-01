5eabe14514434a4439634c7d8615a59f1dede63c
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-07-02 01:21:44 +0700

    Add Vue Js 3 and Bootstrap 5

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import { createApp } from 'vue'
+import Welcome from './components/Welcome.vue'
+
+const app = createApp({})
+
+app.component('welcome', Welcome)
+
+app.mount('#app')
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="vue-test">
+                    <button class="btn btn-primary m-4">Click Me!</button>
+                    <h1>Vue JS 3 x Laravel 8 x Bootstrap 5</h1>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .vue-test{
+        display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+    }
+</style>
