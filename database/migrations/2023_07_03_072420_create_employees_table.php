a3a4a296131230edafba17d1366b0d2a10834b23
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-07-04 13:05:01 +0700

    Database Migration

---
+++ database/migrations/2023_07_03_072420_create_employees_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('nik');
+            $table->string('npwp');
+            $table->string('full_name');
+            $table->string('tempat_lahir');
+            $table->string('gender');
+            $table->text('address');
+            $table->string('jabatan');
+            $table->dateTime('start_masa_kerja');
+            $table->dateTime('end_masa_kerja');
+            $table->boolean('is_active');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
