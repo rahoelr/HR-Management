432fecba2674da95c5a78da90102288be7f55840
Mumtaza15 <ahmad.fatha1502@mail.ugm.ac.id>
2023-07-14 13:05:30 +0700

    up layouting desain

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,40 +10,43 @@
 @stop
 
 @section('menu')
-<!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active bg-menu bg-menu-active">
         <a class="nav-link" href="/dashboard">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></a>
+            <span>Dashboard</span>
+        </a>
     </li>
 
-<!-- Nav Item - Timesheet -->
-    <li class="nav-item">
+    <!-- Nav Item - Timesheet -->
+    <li class="nav-item bg-menu">
         <a class="nav-link" href="/timesheet">
-            <i class="fas fa-fw fa-chart-area"></i>
-                <span>Timesheet</span></a>
+            <i class="fas fa-fw fa-calendar-days" style="color:#3A3A3A;"></i>
+            <span style="color:#3A3A3A;">Timesheet</span>
+        </a>
     </li>
 
-<!-- Nav Item - Cuti -->
-    <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-                <span>Pengajuan Cuti</span></a>
+    <!-- Nav Item - Cuti -->
+    <li class="nav-item bg-menu">
+        <a class="nav-link" href="/timesheet">
+            <i class="fas fa-fw fa-clock" style="color:#3A3A3A;"></i>
+            <span style="color:#3A3A3A;">Pengajuan Cuti</span>
+        </a>
     </li>
 @stop
 
 @section('content')
     <div id="app">
         <div>
-            <dashboard/>
+            <dashboard />
         </div>
     </div>
 @stop
 
 @push('javascript')
-    <!-- <script src="{{asset('js/chart-bar.js')}}" type="text/javascript"></script> -->
-    <script src="{{asset('js/Chart.js')}}" type="text/javascript"></script>
+    <!-- <script src="{{ asset('js/chart-bar.js') }}" type="text/javascript"></script> -->
+    <script src="{{ asset('js/Chart.js') }}" type="text/javascript"></script>
 
-    <script type="text/javascript" src="{{ URL::asset ('js/chart-bar.js') }}"></script>
-    <!-- <script type="text/javascript" src="{{ URL::asset ('js/Chart.js') }}"></script> -->
+    <script type="text/javascript" src="{{ URL::asset('js/chart-bar.js') }}"></script>
+    <!-- <script type="text/javascript" src="{{ URL::asset('js/Chart.js') }}"></script> -->
 @endpush
--- resources/views/timesheet.blade.php
+++ resources/views/timesheet.blade.php
@@ -5,41 +5,44 @@
 @stop
 
 @section('css')
-    <!-- <link href="{{asset('css/timesheet.css')}}"></script> -->
-    <link href="css/timesheet.css" rel="stylesheet"/>
+    <!-- <link href="{{ asset('css/timesheet.css') }}"></script> -->
+    <link href="css/timesheet.css" rel="stylesheet" />
 @stop
 
 @section('menu')
-<!-- Nav Item - Dashboard -->
-    <li class="nav-item">
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item bg-menu">
         <a class="nav-link" href="/dashboard">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></a>
+            <span style="color:#3A3A3A;">Dashboard</span>
+        </a>
     </li>
 
-<!-- Nav Item - Timesheet -->
-    <li class="nav-item active">
+    <!-- Nav Item - Timesheet -->
+    <li class="nav-item active bg-menu bg-menu-active">
         <a class="nav-link" href="/timesheet">
-            <i class="fas fa-fw fa-chart-area"></i>
-                <span>Timesheet</span></a>
+            <i class="fas fa-fw fa-calendar-days" style="color:#3A3A3A;"></i>
+            <span>Timesheet</span>
+        </a>
     </li>
 
-<!-- Nav Item - Cuti -->
-    <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-                <span>Pengajuan Cuti</span></a>
+    <!-- Nav Item - Cuti -->
+    <li class="nav-item bg-menu">
+        <a class="nav-link" href="/timesheet">
+            <i class="fas fa-fw fa-clock" style="color:#3A3A3A;"></i>
+            <span style="color:#3A3A3A;">Pengajuan Cuti</span>
+        </a>
     </li>
 @stop
 
 @section('content')
     <div id="app">
         <div>
-            <timesheet/>
+            <timesheet />
         </div>
     </div>
 @stop
 
 @section('javascript')
-    
+
 @stop
